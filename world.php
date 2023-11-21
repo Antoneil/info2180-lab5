8f2acbdb8b89aac3c4e29448ce53ca3eaa2f0a12
Antoneil Singh <antoneilsingh@gmail.com>
2023-11-21 00:00:03 -0500

    Added Cities

--- world.php
+++ world.php
@@ -6,16 +6,37 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
 $country = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_STRING);
-
-// Check if context is cities
 $context = filter_input(INPUT_GET, 'context', FILTER_SANITIZE_STRING);
-if ($context === 'cities') {
-    
-} else {
-    $countrysearch = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-    $countryresult = $countrysearch->fetchAll(PDO::FETCH_ASSOC);
 
-    // Display the search results
+if ($context === 'cities') {
+    $citySearch = $conn->query("SELECT cities.name AS city_name, cities.district, cities.population FROM cities 
+                               JOIN countries ON cities.country_code = countries.code
+                               WHERE countries.name LIKE '%$country%'");
+    $cityResult = $citySearch->fetchAll(PDO::FETCH_ASSOC);
+
+    // Display the search results for cities
+    ?>
+    <table id="TableWithCities">
+        <tr>
+            <th><?= 'Name'; ?></th>
+            <th><?= 'District'; ?></th>
+            <th><?= 'Population'; ?></th>
+        </tr>
+
+        <?php foreach ($cityResult as $row): ?>
+            <tr>
+                <td><?= $row['city_name']; ?></td>
+                <td><?= $row['district']; ?></td>
+                <td><?= $row['population']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+    <?php
+} else {
+    $countrySearch = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $countryResult = $countrySearch->fetchAll(PDO::FETCH_ASSOC);
+
+    // Display the search results for countries
     ?>
     <table id="TableWithCountries">
         <tr>
@@ -25,7 +46,7 @@ if ($context === 'cities') {
             <th><?= 'Head of State'; ?></th>
         </tr>
 
-        <?php foreach ($countryresult as $row): ?>
+        <?php foreach ($countryResult as $row): ?>
             <tr>
                 <td><?= $row['name']; ?></td>
                 <td><?= $row['continent']; ?></td>
